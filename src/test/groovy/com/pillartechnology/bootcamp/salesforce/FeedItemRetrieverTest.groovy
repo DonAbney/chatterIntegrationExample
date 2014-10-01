d1c124e967817cec1afcaecc76132539d32caf66
Paul Manning <psmanningjr@gmail.com>
2014-10-01 12:10:41 -0400

    Added createTopicId req and create messagesFromTopicId request

--- src/test/groovy/com/pillartechnology/bootcamp/salesforce/FeedItemRetrieverTest.groovy
+++ src/test/groovy/com/pillartechnology/bootcamp/salesforce/FeedItemRetrieverTest.groovy
@@ -1,10 +1,14 @@
 package com.pillartechnology.bootcamp.salesforce
 
+import org.apache.http.HttpResponse
+import org.apache.http.HttpVersion
 import org.apache.http.client.methods.HttpUriRequest
+import org.apache.http.message.BasicHttpResponse
+import org.apache.http.message.BasicStatusLine
 
 class FeedItemRetrieverTest extends GroovyTestCase {
 
-	private FeedItemRetriever feedItemRetriever;
+	private FeedItemRetrieverImpl feedItemRetriever;
 
 	void initFeedItemRetriever() {
 		feedItemRetriever = new FeedItemRetrieverImpl()
@@ -37,10 +41,24 @@ class FeedItemRetrieverTest extends GroovyTestCase {
 		}
 	}
 
-	void testCreateFeedItemHttpRequestWithCorrectTopic() {
-		FeedItemRetrieverImpl feedItemRetrieverImpl = new FeedItemRetrieverImpl()
-		HttpUriRequest request = feedItemRetrieverImpl.createFeedItemHttpRequest("testUrl", "token", "topic")
+	void testCreateTopicIdHttpRequestWithCorrectTopic() {
+		initFeedItemRetriever()
+		HttpUriRequest request = feedItemRetriever.createTopicIdHttpRequest("testUrl", "token", "topic")
 		
 		assertEquals("testUrl/services/data/v31.0/connect/topics?exactMatch=true&q=topic", request.getURI().toString());
 	}
+	
+	void testParseTopicIdFromTopicIdResponse() {
+		initFeedItemRetriever()
+		HttpResponse response = new BasicHttpResponse(new BasicStatusLine(HttpVersion.HTTP_1_1, 201, "test"))
+		def topicId = feedItemRetriever.parseTopicIdFromResponse(response)
+		assertEquals("targetTopicId",topicId)
+	}
+
+	void testCreateMessagesFromTopicIdHttpRequestWithCorrectTopicId() {
+		initFeedItemRetriever()
+		HttpUriRequest request = feedItemRetriever.createMessagesFromTopicIdHttpRequest("testUrl", "token", "topicid")
+		assertEquals("testUrl/services/data/v31.0/chatter/feeds/topics/topicid/feed-items", request.getURI().toString());
+	}
+	
 }
